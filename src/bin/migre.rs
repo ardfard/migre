3dfbad125446b46a607bd1647619dd586ac97017
Ardin Fardiansyah <fardiansyah.ardin@yahoo.com>
2020-04-12 14:22:54 +0700

    use async-std for concurrency

--- src/bin/migre.rs
+++ src/bin/migre.rs
@@ -1,7 +1,6 @@
-use futures_util::future::join_all;
+use async_std::{fs::File, io, task};
+use futures_util::io::AsyncReadExt;
 use structopt::StructOpt;
-use tokio::fs::File;
-use tokio::io::{self, AsyncReadExt};
 
 #[derive(Debug, StructOpt)]
 #[structopt(
@@ -13,20 +12,22 @@ struct CLIArg {
     config: String,
 }
 
-#[tokio::main]
-async fn main() -> io::Result<()> {
+fn main() -> io::Result<()> {
     let opt = CLIArg::from_args();
-    let mut conf_file = match File::open(&opt.config).await {
-        Err(err) => panic!("Couldn't open {}: {}", opt.config, err.to_string()),
-        Ok(file) => file,
-    };
-    let mut conf_str = String::new();
-    match conf_file.read_to_string(&mut conf_str).await {
-        Err(err) => panic!("couldn't read {}: {}", opt.config, err.to_string()),
-        Ok(_) => {
-            let config = migre::Config::from_config_str(&conf_str);
-            migre::start(config).await;
+    task::block_on(async {
+        let mut conf_file = match File::open(&opt.config).await {
+            Err(err) => panic!("Couldn't open {}: {}", opt.config, err.to_string()),
+            Ok(file) => file,
+        };
+        let mut conf_str = String::new();
+        match conf_file.read_to_string(&mut conf_str).await {
+            Err(err) => panic!("couldn't read {}: {}", opt.config, err.to_string()),
+            Ok(_) => {
+                let config = migre::Config::from_config_str(&conf_str);
+                migre::start(config).await;
+            }
         }
-    }
+    });
+
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
+use async_std::{
+    io,
+    net::{TcpListener, TcpStream},
+    prelude::*,
+    task,
+};
 use futures_util::future::join_all;
 use serde::Deserialize;
-use std::future::Future;
 use std::io::ErrorKind;
 use std::sync::Arc;
-use tokio::net::tcp::{ReadHalf, WriteHalf};
-use tokio::net::{TcpListener, TcpStream};
-use tokio::prelude::*;
-use tokio::task::JoinHandle;
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
@@ -24,31 +25,39 @@ impl Config {
 
 async fn process_incoming(upstream_addrs: Arc<Vec<String>>, mut client_stream: TcpStream) {
     let mut join_handles = Vec::new();
-    let (mut client_read, mut client_write) = client_stream.split();
     for addr in upstream_addrs.iter() {
-        let upstream = String::from(addr);
-        join_handles.push(tokio::spawn(async {
-            let target = TcpStream::connect(upstream).await.unwrap();
+        let addr_str = String::from(addr);
+        join_handles.push(task::spawn(async move {
+            println!("connecting to {}", addr_str);
+            let target = TcpStream::connect(addr_str)
+                .await
+                .expect("Error connection to target!");
             target
         }));
     }
 
-    let mut write_halfes: Vec<TcpStream> = join_all(join_handles)
-        .await
-        .into_iter()
-        .map(|x| x.unwrap())
-        .collect();
+    let mut upstreams: Vec<TcpStream> = join_all(join_handles).await.into_iter().collect();
+
+    let mut clone_client = client_stream.clone();
+    let mut clone_target = upstreams[0].clone();
+
+    task::spawn(async move { io::copy(&mut clone_target, &mut clone_client).await });
+
+    for upstream in &upstreams[1..] {
+        let mut clone_tx = upstream.clone();
+        task::spawn(async move { io::copy(&mut clone_tx, &mut io::sink()).await });
+    }
 
     loop {
         let mut buf = [0 as u8; 255];
-        let len = match client_read.read(&mut buf).await {
+        let len = match client_stream.read(&mut buf).await {
             Ok(0) => break,
             Ok(len) => len,
             Err(ref e) if e.kind() == ErrorKind::Interrupted => continue,
             Err(_) => break,
         };
-        let mut handles = Vec::with_capacity(write_halfes.len());
-        for upstream in write_halfes.iter_mut() {
+        let mut handles = Vec::with_capacity(upstreams.len());
+        for upstream in upstreams.iter_mut() {
             handles.push(upstream.write_all(&buf[..len]));
         }
         join_all(handles).await;
@@ -56,7 +65,7 @@ async fn process_incoming(upstream_addrs: Arc<Vec<String>>, mut client_stream: T
 }
 
 pub async fn start(config: Config) {
-    let mut listener = TcpListener::bind(config.listen_addr)
+    let listener = TcpListener::bind(config.listen_addr)
         .await
         .expect("unable to bind address");
     println!("Start Listening on {}", listener.local_addr().unwrap());
@@ -65,7 +74,7 @@ pub async fn start(config: Config) {
         let (client, client_addr) = listener.accept().await.unwrap();
         println!("incoming connection from: {}", client_addr);
         let cloned_upstreams = upstreams.clone();
-        tokio::spawn(async move {
+        task::spawn(async move {
             process_incoming(cloned_upstreams, client).await;
         });
         if config.run_once == Some(true) {
@@ -77,8 +86,10 @@ pub async fn start(config: Config) {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::io::{BufRead, BufReader};
-    use std::net::Shutdown;
+    use async_std::{net::Shutdown, task};
+    use futures_util::future;
+    use futures_util::io::BufReader;
+    use std::time::Duration;
 
     static CONF_STR: &str = r#"
         listen_addr = "127.0.0.1:12345"
@@ -96,40 +107,39 @@ mod tests {
         assert_eq!(config.worker_pool_size, Some(8));
     }
 
-    fn dummy_tcp_service(listen_addr: &str) {
-        let listener = TcpListener::bind(listen_addr);
-        let (mut stream, _) = listener.accept().unwrap();
+    async fn dummy_tcp_service(listen_addr: &str) {
+        let listener = TcpListener::bind(listen_addr).await.unwrap();
+        let (mut stream, _) = listener.accept().await.unwrap();
         let mut buffer = BufReader::new(&stream);
         let mut payload = String::new();
         println!("start listening dummy");
-        buffer.read_line(&mut payload).unwrap();
+        buffer.read_line(&mut payload).await.unwrap();
         println!("payload incoming: {}", payload);
-        stream.write_all(payload.as_bytes()).unwrap();
-        stream.flush().unwrap();
+        stream.write_all(payload.as_bytes()).await.unwrap();
+        stream.flush().await.unwrap();
     }
 
     #[test]
     fn proxy_tcp_request() {
-        let config = Config::from_config_str(&CONF_STR);
-        let thr1 = std::thread::spawn(move || {
-            dummy_tcp_service("127.0.0.1:8000");
+        task::block_on(async {
+            let config = Config::from_config_str(&CONF_STR);
+            let thr1 = task::spawn(async {
+                dummy_tcp_service("127.0.0.1:8000").await;
+            });
+            let thr2 = task::spawn(async {
+                dummy_tcp_service("127.0.0.1:8001").await;
+            });
+            let thr = task::spawn(async { start(config).await });
+            task::sleep(Duration::from_millis(100)).await;
+            let mut stream = TcpStream::connect("127.0.0.1:12345").await.unwrap();
+            stream.write_all("hello\n".as_bytes()).await.unwrap();
+            stream.flush().await.unwrap();
+            stream.shutdown(Shutdown::Write).unwrap();
+            let mut result = String::new();
+            stream.read_to_string(&mut result).await.unwrap();
+            assert_eq!(result, "hello\n");
+            future::join3(thr, thr1, thr2).await;
         });
-        let thr2 = std::thread::spawn(move || {
-            dummy_tcp_service("127.0.0.1:8001");
-        });
-        let thr = std::thread::spawn(move || start(config));
-        std::thread::sleep(std::time::Duration::from_millis(10));
-
-        let mut stream = TcpStream::connect("127.0.0.1:12345").unwrap();
-        stream.write_all("hello\n".as_bytes()).unwrap();
-        stream.flush().unwrap();
-        stream.shutdown(Shutdown::Write).unwrap();
-        let mut result = String::new();
-        stream.read_to_string(&mut result).unwrap();
-        assert_eq!(result, "hello\n");
-        thr.join().unwrap();
-        thr1.join().unwrap();
-        thr2.join().unwrap();
     }
 
     #[test]
